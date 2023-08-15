bb099e4a97ceb7ed4cd12b58950f3476de77ee46
Luiz Tozati <luiztozati77@gmail.com>
2023-08-15 13:40:25 -0300

    feat: Adicionar rota /welcome

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,6 +1,7 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class Controller {
   @GetMapping("/message")
   public String message() {
     return "Hello World!";
+  }
+
+  @GetMapping("/welcome/{name}")
+  public String welcome(@PathVariable String name){
+    return String.format("Welcome %s", name);
   }
 }
