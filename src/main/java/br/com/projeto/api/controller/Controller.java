1a47e1ccf4e16ac373f51f1ccfd7234f19123099
Luiz Tozati <luiztozati77@gmail.com>
2023-08-16 22:08:37 -0300

    feat: Adicionar rota /pessoa

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -2,7 +2,11 @@ package br.com.projeto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.model.Person;
 
 @RestController
 public class Controller {
@@ -15,5 +19,10 @@ public class Controller {
   @GetMapping("/welcome/{name}")
   public String welcome(@PathVariable String name){
     return String.format("Welcome %s", name);
+  }
+
+  @PostMapping("/pessoa")
+  public Person person(@RequestBody Person p){
+    return p;
   }
 }
