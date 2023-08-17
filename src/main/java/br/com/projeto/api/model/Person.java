5d85926e0faa7d3a0dac34c128b68820222638da
Luiz Tozati <luiztozati77@gmail.com>
2023-08-16 22:39:13 -0300

    chore: Adicionar anotações à classe de entidade Person

--- src/main/java/br/com/projeto/api/model/Person.java
+++ src/main/java/br/com/projeto/api/model/Person.java
@@ -1,9 +1,19 @@
 package br.com.projeto.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "people")
 public class Person {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String nome;
   private int idade;
