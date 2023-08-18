f8015b60f916dc992a7442795b65fa156b72759f
Luiz Tozati <luiztozati77@gmail.com>
2023-08-18 17:52:51 -0300

    feat: Adicionar método selectAll para rota /api

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,5 +1,7 @@
 package br.com.projeto.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,11 @@ public class Controller {
   @PostMapping("/api")
   public Person register(@RequestBody Person obj){
     return action.save(obj);
+  }
+
+  @GetMapping("/api")
+  public List<Person> selectAll(){
+    return action.findAll();
   }
 
   @GetMapping("/message")
