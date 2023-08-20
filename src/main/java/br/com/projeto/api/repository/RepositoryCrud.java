8b683b72e0e7aa69c9dbaa3ab1bade4bbd0749ac
Luiz Tozati <luiztozati77@gmail.com>
2023-08-19 21:01:15 -0300

    feat: Adicionar método sumAges na interface RepositoryCrud

--- src/main/java/br/com/projeto/api/repository/RepositoryCrud.java
+++ src/main/java/br/com/projeto/api/repository/RepositoryCrud.java
@@ -2,19 +2,30 @@ package br.com.projeto.api.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.projeto.api.model.Person;
 
 @Repository
-public interface RepositoryCrud extends CrudRepository<Person, Integer>{
-  
+public interface RepositoryCrud extends CrudRepository<Person, Integer> {
+
   List<Person> findAll();
+
   Person findById(int id);
+
   List<Person> findByOrderByName();
+
   List<Person> findByNameOrderByAgeDesc(String name);
+
   List<Person> findByNameContaining(String term);
+
   List<Person> findByNameStartsWith(String term);
+
   List<Person> findByNameEndsWith(String term);
+
+  @Query(value = "SELECT SUM(age) FROM people", nativeQuery = true)
+  int sumAges();
+
 }
