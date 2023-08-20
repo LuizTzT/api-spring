e77a15495f68a7b96c6093beda8f93484d389628
Luiz Tozati <luiztozati77@gmail.com>
2023-08-20 16:13:39 -0300

    chore: Adicionar atributo 'message' à classe ServiceCrud

--- src/main/java/br/com/projeto/api/service/ServiceCrud.java
+++ src/main/java/br/com/projeto/api/service/ServiceCrud.java
@@ -1,8 +1,14 @@
 package br.com.projeto.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.projeto.api.model.Message;
 
 @Service
 public class ServiceCrud {
-  
+
+  @Autowired
+  private Message message;
+
 }
