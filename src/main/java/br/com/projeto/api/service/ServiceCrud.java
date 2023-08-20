3964c93126049292e05c4d5afafbc8281731e5d0
Luiz Tozati <luiztozati77@gmail.com>
2023-08-20 17:51:17 -0300

    feat: Adicionar método 'register' para cadastrar pessoas com validações de nome idade

--- src/main/java/br/com/projeto/api/service/ServiceCrud.java
+++ src/main/java/br/com/projeto/api/service/ServiceCrud.java
@@ -1,14 +1,33 @@
 package br.com.projeto.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.projeto.api.model.Message;
+import br.com.projeto.api.model.Person;
+import br.com.projeto.api.repository.RepositoryCrud;
 
 @Service
 public class ServiceCrud {
 
   @Autowired
   private Message message;
+
+  @Autowired
+  private RepositoryCrud action;
+
+  public ResponseEntity<?> register(Person obj) {
+    if (obj.getName().equals("")) {
+      message.setMessage("O nome precisa ser informado.");
+      return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    } else if (obj.getAge() < 0) {
+      message.setMessage("Informe uma idade maior que zero.");
+      return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    } else {
+      return new ResponseEntity<>(action.save(obj), HttpStatus.CREATED);
+    }
+  }
 
 }
