912f12c1a7a13cefce1e8d3a4120f8bf0970640c
Luiz Tozati <luiztozati77@gmail.com>
2023-08-21 15:46:14 -0300

    feat: Adicionar validações de entrada na entity Client

--- src/main/java/br/com/projeto/api/model/Client.java
+++ src/main/java/br/com/projeto/api/model/Client.java
@@ -5,16 +5,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
-@Entity
 @Data
+@Entity
 @Table(name = "clients")
 public class Client {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+
+  @NotEmpty(message = "Informe um nome.")
   private String name;
+
+  @Email(message = "Informe um e-mail válido.")
   private String email;
 }
